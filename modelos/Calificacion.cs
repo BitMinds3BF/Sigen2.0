f81dddf0d6980302afcee23a08149c40fc648ea9
Chelo <cheloartigas1993@hotmail.com>
2024-10-28 16:15:04 -0300

    Nuevo dto y repositorio correspondiente
    Agregado el DTO Calificaciones para obtener
    las calificaciones de un usuario.
    Agregado el repositorio correspondiente para
    este DTO.

---
+++ modelos/Calificacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITMINDS.modelos
+{
+    public class Calificacion
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public string Fecha { get; set; }
+        public int PuntajeObtenido { get; set; }
+    }
+}
